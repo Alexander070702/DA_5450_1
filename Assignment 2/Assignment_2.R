8b3991bbe8d33bb8edba5527fa98a07c3a360caa
nevoljica <128471026+nevoljica@users.noreply.github.com>
2023-03-28 15:26:29 +0200

    Update Assignment_2.R
    adding comments

--- Assignment 2/Assignment_2.R
+++ Assignment 2/Assignment_2.R
@@ -9,11 +9,15 @@ churndat$churn <- ifelse(churndat$churn == "Yes", 1, 0)
 churndat$internationalplan <- ifelse(churndat$internationalplan == "yes", 1, 0)
 churndat$voicemailplan <- ifelse(churndat$voicemailplan == "yes", 1, 0)
 
+# 2 Splitting the data to training set and test set
+
 # Split the data into training and testing sets, with 80% for training and 20% for testing
 set.seed(123) # for reproducibility
 train_index <- createDataPartition(churndat$churn, p = 0.8, list = FALSE)
 train <- churndat[train_index, ]
 test <- churndat[-train_index, ]
+
+# 3 Full logistic regression model
 
 # Fit a logistic regression model to the training data using all variables as predictors
 mlm <- glm(churn ~ ., data = train, family = binomial())
@@ -21,12 +25,26 @@ mlm <- glm(churn ~ ., data = train, family = binomial())
 # Print a summary of the logistic regression model
 summary(mlm)
 
+# The significant variables in this model are: internationalplan, voicemailplan, numbervmailmessages, totalintlcalls and numbercustomerservicecalls.
+
+
+# 4 Logistic model stepwise analysis
+
 # Perform stepwise variable selection on the logistic regression model using both forward and backward selection
 # The 'trace' argument controls the amount of output produced during the stepwise procedure
 mlm_step <- step(mlm, direction = "both", trace = FALSE)
 
 # Print a summary of the stepwise logistic regression model
 summary(mlm_step)
+
+# Variables in the final model are : accountlength, internationalplan, voicemailplan, numbervmailmessages, totaldayminutes, 
+# totaleveminutes, totalnightminutes, totalintlcalls, totalintlcharge and numbercustomerservicecalls.
+
+# The coefficient of totaldayminutes suggests that there is a positive correlation between minutes talked and probability of leaving the company.
+# Similarly, the coefficient of voicemailplan suggests a positive correlation between having a plan including a voicemail and the probability of leaving the company.
+
+
+#5
 
 # Calculate the predicted probabilities and predicted classes of the testing data using the stepwise logistic regression model
 test$prob_churn_step <- predict(mlm_step, newdata = test, type = "response")
