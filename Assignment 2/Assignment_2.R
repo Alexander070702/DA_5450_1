26a315c0bb2508141cfc9ced5dd9abaf06a0679f
Alexander Hillisch <alexanderhillisch@MacBook-Pro-von-Alexander.local>
2023-03-28 14:56:28 +0200

    Comments with Chat

--- Assignment 2/Assignment_2.R
+++ Assignment 2/Assignment_2.R
@@ -1,25 +1,29 @@
-library ("caret")
+# Load the caret package for data partitioning
+library("caret")
 
+# Load the churn dataset from a URL and convert the churn variable to binary
 churndat <- read.csv(url("https://statmath.wu.ac.at/~malsiner/datasets/churn.csv"))
 churndat$churn <- ifelse(churndat$churn == "Yes", 1, 0)
+
+# Convert the internationalplan and voicemailplan variables to binary
 churndat$internationalplan <- ifelse(churndat$internationalplan == "yes", 1, 0)
 churndat$voicemailplan <- ifelse(churndat$voicemailplan == "yes", 1, 0)
 
-
-
-
-
-#2
-set.seed(123)
+# Split the data into training and testing sets, with 80% for training and 20% for testing
 train_index <- createDataPartition(churndat$churn, p = 0.8, list = FALSE)
 train <- churndat[train_index, ]
 test <- churndat[-train_index, ]
 
-#3
-mlm = glm(churn ~ .,data = train, family = binomial())
+# Fit a logistic regression model to the training data using all variables as predictors
+mlm <- glm(churn ~ ., data = train, family = binomial())
+
+# Print a summary of the logistic regression model
 summary(mlm)
 
-#4
-mlm_step = step(mlm, direction = "both", trace = FALSE)
+# Perform stepwise variable selection on the logistic regression model using both forward and backward selection
+# The 'trace' argument controls the amount of output produced during the stepwise procedure
+mlm_step <- step(mlm, direction = "both", trace = FALSE)
+
+# Print a summary of the stepwise logistic regression model
 summary(mlm_step)
 
